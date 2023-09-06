d069203d1c77366a7311b9f214cd4e02e6faa3c5
Hicham Abboud <66979859+hichamabboud@users.noreply.github.com>
2023-09-06 14:21:57 -0400

    creating lodge hopper project

---
+++ components/Footer.js
@@ -0,0 +1,6 @@
+
+export default function Footer() {
+  return (
+    <div className='bg-gray-400 h-24'>Footer</div>
+  )
+}
---
+++ components/ResidentsShow.js
@@ -0,0 +1,6 @@
+
+export default function ResidentsShow() {
+  return (
+    <div>ResidentsShow</div>
+  )
+}
